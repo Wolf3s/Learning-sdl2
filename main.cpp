daeb42ae4412b5fe2cdbd7b359a672012ddb9fde
Wolf3s <75867486+wolfysdl@users.noreply.github.com>
2022-01-30 19:06:29 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /******************************************************
-** SDL2 Console hello world                          **
-** Copyright André Guilherme Mendes da Luz Bastos   **
+** SDL2 Console message                              **
+** Copyright André Guilherme Mendes da Luz Bastos    **
 ** Made with C++ 20 and tested on code::blocks 20.03 **
 *******************************************************/
 
