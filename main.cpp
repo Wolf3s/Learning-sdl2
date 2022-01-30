780c90af5e31c2e7c999f8beeace68b5ff634cbe
Wolf3s <75867486+wolfysdl@users.noreply.github.com>
2022-01-30 18:55:23 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/******************************************************
+** SDL2 Console hello world                          **
+** Copyright Andr� Guilherme Mendes da Luz Bastos   **
+** Made with C++ 20 and tested on code::blocks 20.03 **
+*******************************************************/
+
+//iostream and sdl libraries
+#include <SDL.h>
+#include <iostream>
+
+//Definitions of screen
+#define WIDTH 640
+#define HEIGHT 480
+
+//Normal main declaration
+int main(int argv, char* args[])
+{
+  std::cout << "init video";
+  //inits video and then creates the screen
+  SDL_Init( SDL_INIT_VIDEO );
+  SDL_CreateWindow("Init", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOWEVENT);
+  std::cout << "\nExiting";
+  //exits
+  SDL_Quit();
+  return 0;
+}
+
+
